d340e48e304bda38c6f20cefdae70dd6212f4cb1
Ruslan <ruslanchallenger@gmail.com>
2019-08-07 11:24:25 +0500

    MATH V1

---
+++ core/src/Other/HUD.java
@@ -0,0 +1,23 @@
+package Other;
+
+import com.awprecords.zombieclicker.ZombieClicker;
+
+public class HUD {
+
+    private final ZombieClicker zombieClicker;
+
+    public HUD(ZombieClicker zc){
+        zombieClicker = zc;
+
+    }
+
+    public void render(){
+
+    }
+
+    public void dispose(){
+
+    }
+
+
+}
